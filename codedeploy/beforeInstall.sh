70990d45cf130f790bc750002d296fa62a991bc5
Mengchen Gao <gao.mengc@husky.neu.edu>
2020-03-12 16:56:49 -0400

    fixed kill 8080

--- codedeploy/beforeInstall.sh
+++ codedeploy/beforeInstall.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -v
 
 # shellcheck disable=SC2046
-sudo kill -9 $(lsof -t -i:8080)
+if [ -z $(lsof -t -i:8080) ]
+then
+      echo "nothing runs on 8080"
+else
+      sudo kill -9 $(lsof -t -i:8080)
+fi
+
 source /env/properties.sh
